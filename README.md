e4472e82ca957f9e6a25d0453f1b23aca7e4c134
Joselito <jaguardado123@gmail.com>
2024-10-02 12:50:19 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# hw4
+
+For this homework you will need to refactor some code.
+
+The app is a simple task list. However, the task widgets have been copied and pasted :( with small modifications to each.
+
+
+<img src="screenshots/initial_app.png" width="40%">
+
+**initial screenshot of app with 3 hardcoded tasks**
+
+You first will need to refactor out the task widget into a class. This class should take in the title and the initial state (checked/unchecked) of the task. 
+
+```dart
+
+Task(
+    title: 'clean room',
+    checked: false,
+)
+
+```
+
+
+After refactoring the Task Widget replace the [ListView](https://api.flutter.dev/flutter/widgets/ListView-class.html) to use [ListView.builder()](https://flutter.dev/docs/cookbook/lists/long-lists) to dynamically make tasks.
+
+Two arrays have been provided **taskTitles** and **taskStates** they  hold the title and inital state of each task.
+You will need to used this arrays in the [ListView.builder()](https://flutter.dev/docs/cookbook/lists/long-lists) to generate the tasks.
+
+```dart
+
+Task(
+    title: taskTitles[index],
+    checked: tasksState[index],
+)
+
+```
+
+
+<img src="screenshots/final_result.png" width="40%">
+
+**end result with app using listview.builder to generate all the task in the array**
+
+# Hint:
+
+When dealing with Stateful Widgets, let's say you have a Stateful Widget called Task and you pass information to it via the constructor.
+
+To access variables in the Task class, use `widget.whatever` in the _TaskState class. 
+
+```dart
+
+class Task extends StatefulWidget {
+  // Make sure to use final keyword
+  final String whatever;
+
+  // Constructor
+  const Task({Key? key, required this.whatever});
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(widget.whatever);
+  }
+}
+
+```
+
+For Flutter Version 3, we can't update
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<String> taskTitles = [
+    'Understand Code',
+    'Figure out duplication',
+    'Refactor',
+    'Add comments',
+    'commit code',
+    'push to github',
+  ];
+
+  List<bool> taskStates = [
+    true,
+    false,
+    true,
+    false,
+    false,
+    false,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: const Text('Your Tasks'),
+      ),
+      body: Container(
+        color: Colors.teal,
+        // REFACTOR INTO ListView.builder
+        child: ListView(children: <Widget>[
+          // REFACTOR OUT INTO STATEFUL TASK WIDGETS
+          Card(
+            child: CheckboxListTile(
+              value: taskStates[0],
+              onChanged: (newValue) {
+                setState(() {
+                  taskStates[0] = newValue ?? false;
+                });
+              },
+              activeColor: Colors.teal,
+              title: Text(taskTitles[0],
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontStyle:
+                        taskStates[0] ? FontStyle.italic : FontStyle.normal,
+                    color: taskStates[0] ? Colors.grey : Colors.black,
+                  )),
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+          ),
+          Card(
+            child: CheckboxListTile(
+              value: taskStates[1],
+              onChanged: (newValue) {
+                setState(() {
+                  taskStates[1] = newValue ?? false;
+                });
+              },
+              activeColor: Colors.teal,
+              title: Text(taskTitles[1],
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontStyle:
+                        taskStates[1] ? FontStyle.italic : FontStyle.normal,
+                    color: taskStates[1] ? Colors.grey : Colors.black,
+                  )),
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+          ),
+          Card(
+            child: CheckboxListTile(
+              value: taskStates[2],
+              onChanged: (newValue) {
+                setState(() {
+                  taskStates[2] = newValue ?? false;
+                });
+              },
+              activeColor: Colors.teal,
+              title: Text(taskTitles[2],
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontStyle:
+                        taskStates[2] ? FontStyle.italic : FontStyle.normal,
+                    color: taskStates[2] ? Colors.grey : Colors.black,
+                  )),
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
